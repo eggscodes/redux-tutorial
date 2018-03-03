b9ff76053e2aadb14f5768c4c08d56150a0d492f
Michael Eggers <michael@meggers.me>
2018-03-03 08:40:57 -0500

    added .idea to gitignore

--- src/index.js
+++ src/index.js
@@ -15,10 +15,12 @@ class App extends Component {
       videos: [],
       selectedVideo: null
     };
+  }
 
-    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
       this.setState({
-        videos,
+        videos: videos,
         selectedVideo: videos[0]
       }); // same as this.setState({ videos: videos });
     });
@@ -27,7 +29,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
